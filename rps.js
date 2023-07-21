88ff821ba7e23f0b53184e3f9e92ecb1d4a13510
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2023-07-21 01:17:41 -0300

    Add playRound function

--- rps.js
+++ rps.js
@@ -1,3 +1,7 @@
+function playRound (playerSelection, computerSelection) {
+}
+
+
 function getComputerChoice () {
     let choices = ["Rock", "Paper", "Scissors"]
     return choices[Math.floor(Math.random()*choices.length)];
