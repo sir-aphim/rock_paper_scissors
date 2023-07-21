3a60a41bd35b5262d75aa60f68cb81aa5b6e949c
sir-aphim <123579499+sir-aphim@users.noreply.github.com>
2023-07-21 02:00:32 -0300

    Improve possibilities and case insensitivity

--- rps.js
+++ rps.js
@@ -1,24 +1,26 @@
 function playRound (playerSelection, computerSelection) {
-    if (playerSelection === "Paper" && computerSelection === 'Rock') {
+    if (playerSelection.toLowerCase() === "paper" && computerSelection === 'rock') {
         return "You win! Paper wraps rock."
-    } if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    } if (playerSelection.toLowerCase() === "scissors" && computerSelection === "paper") {
         return "You win! Scissors cut paper."
-    } if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    } if (playerSelection.toLowerCase() === "rock" && computerSelection === "scissors") {
         return "You win! Rock beats scissors."
-    } if (playerSelection === "Rock" && computerSelection === 'Paper') {
+    } if (playerSelection.toLowerCase() === "rock" && computerSelection === 'paper') {
         return "You lose! Paper wraps rock."
-    } if (playerSelection === "Paper" && computerSelection === "Scissors") {
+    } if (playerSelection.toLowerCase() === "paper" && computerSelection === "scissors") {
         return "You lose! Scissors cut paper."
-    } if (playerSelection === "Scissors" && computerSelection === "Rock") {
-        return "You win! Rock beats scissors."
-    } if (playerSelection.value === computerSelection.value) {
-        return "It's a tie!"
+    } if (playerSelection.toLowerCase() === "scissors" && computerSelection === "rock") {
+        return "You lose! Rock beats scissors."
+    } if (playerSelection.toLowerCase() === computerSelection) {
+        return `It's a tie between ${playerSelection.toLowerCase()}s!`
+    } else {
+        return "You only have three options..."
     }
 }
 
-let playerSelection = "Paper";
+let playerSelection = "PAPER";
 function getComputerChoice () {
-    let choices = ["Rock", "Paper", "Scissors"]
+    let choices = ["rock", "paper", "scissors"]
     return choices[Math.floor(Math.random()*choices.length)] ;
 }
 
